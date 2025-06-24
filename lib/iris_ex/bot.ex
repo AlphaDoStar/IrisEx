e8a75e4c7bcbad207057d9b74cbc50e719e548d2
AlphaDoStar <alphado2761@gmail.com>
2025-06-24 21:10:35 +0900

    IrisEx.Bot 타입 수정

--- lib/iris_ex/bot.ex
+++ lib/iris_ex/bot.ex
@@ -6,8 +6,8 @@ defmodule IrisEx.Bot do
   @type chat :: %{room: room(), sender: sender(), message: message(), raw: map()}
   @type room :: %{id: String.t(), name: String.t()}
   @type sender :: %{id: String.t(), name: String.t()}
-  @type message :: %{id: String.t(), type: String.t(), content: String.t(), attachment: String.t(), v: String.t()}
-  @callback handle_event(event_type(), chat()) :: any()
+  @type message :: %{id: String.t(), type: String.t(), content: String.t(), attachment: map(), v: map()}
+  @callback handle_event(event_type(), chat()) :: term()
 
   def init, do: IrisEx.Bot.Registry.ensure_started()
   def register(module), do: IrisEx.Bot.Registry.register(module)
